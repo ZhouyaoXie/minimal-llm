71ff0ba95ba2cfed9dedb113280784b88e1954f9
ZhouyaoXie <xiezhouyao99@gmail.com>
2026-04-22 17:19:33 -0700

    set up train script arg parser

--- train/train.py
+++ train/train.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                     prog='Minimal-LLM Train',
                     description='Launches a training run using the input config file')
-    parser.add_argument('-s', '--seed', required = True, help = "random seed for reproducibility")
+    parser.add_argument('-s', '--seed', default = 42, help = "random seed for reproducibility")
     parser.add_argument('-c', '--config', required = True, help="path to training config")
 
     args = parser.parse_args()
